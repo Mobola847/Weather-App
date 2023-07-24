65e28996b660b047bc6a28a2a791f1bbc35f776c
Mobola847 <139622920+Mobola847@users.noreply.github.com>
2023-07-24 23:28:10 +0100

    changed the id

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,7 @@
 let apiKey = "50af66775d6be5518d0c127157771584";
 let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
+
 let now = new Date();
 let currentDate = document.querySelector("#date");
 let h2 = document.querySelector("h2");
@@ -8,7 +9,15 @@ let hours = now.getHours();
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
 let minutes = now.getMinutes();
+
+if(minutes<10){
+  minutes=`0${minutes}`
+}
+if(hours<10){
+  hours=`0${hours}`
+}
 currentDate.innerHTML = `${day} ${hours}:${minutes}`;
+
 
 let humid = document.querySelector("#Humidity");
 let wind = document.querySelector(".Wind");
@@ -16,19 +25,25 @@ let temperature = document.querySelector("#temperature");
 let description = document.querySelector("#description");
 let precipitation = document.querySelector("#Precipitation");
 let current = document.querySelector(".current")
+let icon = document.querySelector("#icon")
+
 
 let h1 = document.querySelector("h1");
 let formText = document.querySelector("#form-text");
 let form = document.querySelector("form");
 
 function changes(response) {
-  h1.innerHTML = `${formText.value}`;
-  humid.innerHTML = `${response.data.main.humidity}`;
+  let weatherCode= `${response.data.weather[0].icon}`
+  description.innerHTML = `${response.data.weather[0].description}`;
+  let iconUrl = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
+  icon.setAttribute("src",iconUrl)
+  icon.setAttribute("alt", response.data.weather[0].description);
+
+  h1.innerHTML = `${response.data.name}`;
   wind.innerHTML = `${response.data.wind.speed}`;
   precipitation.innerHTML = `${response.data.rain["1h"]}`;
   temperature.innerHTML = Math.round(`${response.data.main.temp}`);
-  description.innerHTML = `${response.data.weather[0].description}`;
-  console.log(response);
+  humid.innerHTML = `${response.data.main.humidity}`;
 }
 
 function getResult(event) {
